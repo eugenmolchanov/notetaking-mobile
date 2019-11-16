b850748b3a0c2a230372baae305606bb3daae440
E.Molchanov <yauhenmalchanau@gmail.com>
2019-11-17 01:01:56 +0300

    drawer items icons update

--- LawClient/component/navigator/drawer/item/disciplines/DisciplinesDrawerItems.js
+++ LawClient/component/navigator/drawer/item/disciplines/DisciplinesDrawerItems.js
@@ -1,10 +1,15 @@
 import DrawerItem from '../DrawerItem';
 import React from 'react';
 import { navigateToScreen } from '../DrawerItemUtils';
+import Icon from "react-native-vector-icons/MaterialIcons";
+
+const disciplinesIcon = <Icon name={'format-list-bulleted'} size={25}/>;
+
 
 const DisciplinesDrawerItems = (props) => {
     return (
-        <DrawerItem onPress={navigateToScreen('Disciplines', props.navigation)} name={'Дисциплины'}/>
+        <DrawerItem onPress={navigateToScreen('Disciplines', props.navigation)} name={'Дисциплины'}
+                leftIcon={disciplinesIcon}/>
     );
 };
 
--- LawClient/component/navigator/drawer/item/questions/QuestionsDrawerItems.js
+++ LawClient/component/navigator/drawer/item/questions/QuestionsDrawerItems.js
@@ -1,10 +1,14 @@
 import DrawerItem from '../DrawerItem';
 import React from 'react';
 import { navigateToScreen } from '../DrawerItemUtils';
+import Icon from "react-native-vector-icons/MaterialIcons";
+
+const questionsIcon = <Icon name={'format-list-numbered'} size={25}/>;
 
 const QuestionsDrawerItems = (props) => {
     return (
-        <DrawerItem onPress={navigateToScreen('Questions', props.navigation)} name={'Вопросы'}/>
+        <DrawerItem onPress={navigateToScreen('Questions', props.navigation)} name={'Вопросы'}
+                leftIcon={questionsIcon}/>
     );
 };
 
